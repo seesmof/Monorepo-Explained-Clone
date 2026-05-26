ff751d7af191d62faeb2a7480c3887612b1e6aca
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-26 12:24:40 +0300

    feat: add the files from playground

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+import HeroSection from "@/components/Hero";
+import UndersatingSection from "@/components/Understanding";
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <UndersatingSection />
+    </>
+  );
+}
